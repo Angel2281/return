88ee27b9480a5619c747153c348320095156b237
Angel2281 <32220820+Angel2281@users.noreply.github.com>
2018-07-31 21:08:28 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+ /* SET PASSWORD HERE
+	THIS IS IMPORTANT
+	SO YOU CAN ACCESS
+	THE ADMIN PAGE :) */
+	
+	$user = "Admin"; // username
+	$passw = "MansRp4Life"; // password
+	$apikey = "ABBE995FFEB939A3BAD535F9667E735C";
+
+?>
